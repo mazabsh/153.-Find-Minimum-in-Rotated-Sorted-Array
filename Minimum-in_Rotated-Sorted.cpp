68eee268408d781176eb4f1cedc9848e6451d7dd
Maryam Zabarjad Shiraz <maria.zabarjad@gmail.com>
2025-05-14 15:39:41 -0700

    Create Minimum-in_Rotated-Sorted.cpp

---
+++ Minimum-in_Rotated-Sorted.cpp
@@ -0,0 +1,22 @@
+#include<iostream> 
+#include<vector> 
+using namespace std; 
+
+class Solution{
+public: 
+      int minSearch(vector<int>& nums){
+        int left =0; int right = nums.size()-1; 
+        while(left<right){
+          int mid = left + (right-left)/2; 
+          if(nums[right]<nums[mid]) left = mid+1; 
+          else right = mid; 
+        }
+        return nums[left]; 
+      }
+};
+int main(){
+  vector<int> nums = {3,4,5,1,2}; 
+  Solution sol; 
+  cout << sol.minSearch(nums) << endl; 
+  return 0; 
+}
